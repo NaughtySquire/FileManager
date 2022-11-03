ff5624dd426fe0f7d22fc864c16fe8b892b766b8
Naughty Squire <naughtysquiredev@icloud.com>
2022-11-03 14:23:19 +0300

    "2.1 Bundle. Sandbox. FileManager" completed

--- FileManagerApp/AppDelegate.swift
+++ FileManagerApp/AppDelegate.swift
@@ -1,36 +1,33 @@
-//
-//  AppDelegate.swift
-//  FileManagerApp
-//
-//  Created by Роман Олегович on 03.11.2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithDefaultBackground()
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        do{
+            let documentsURL = try FileManager.default.url(for: .documentDirectory,
+                                                           in: .userDomainMask,
+                                                           appropriateFor: nil,
+                                                           create: false)
+            let fileManagerService = FileManagerService()
+            let navigationController = UINavigationController(rootViewController: DirectoryViewController(fileManagerService, documentsURL))
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        } catch let error{
+            print(error)
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
